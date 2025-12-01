d95a2ab91a11b86d7374df54301db0991ec88a99
Patrick Hammer <patham9@gmail.com>
2025-12-01 18:29:56 +0100

    Update: moved stuff around, now faiss_ffi can go into PeTTa libraries repo

--- build.sh
+++ build.sh
@@ -3,15 +3,3 @@ RUSTFLAGS="-C target-cpu=native" cargo build -p mork_ffi --release
 nm -D ./target/release/libmork_ffi.so | grep rust_mork
 gcc -shared -fPIC -o morklib.so mork.c $(pkg-config --cflags --libs swipl)
 echo "Successfully built mork_ffi"
-
-cd ..
-cd faiss_ffi
-#g++ -fPIC -shared faiss.cpp -o faisslib.so -lfaiss -Wl,-rpath,/usr/local/lib
-g++ -std=c++17 -fPIC -shared faiss.cpp \
-    -o faisslib.so \
-    /usr/local/lib/libfaiss.a \
-    -I/usr/local/include \
-    -lopenblas -lblas -llapack -lgfortran -lm -lpthread \
-    -fopenmp -lgomp \
-    $(pkg-config --cflags --libs swipl)
-echo "Successfully built faiss_ffi"
